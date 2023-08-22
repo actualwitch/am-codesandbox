96002100f4ea2f5e653d58642c80ab27aa66082c
あで <adelaide@fiberplane.com>
2023-08-22 17:14:32 +0200

    Expose metrics

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from autometrics import autometrics
+from prometheus_client import generate_latest
 from quart import Quart
 
 app = Quart(__name__)
@@ -8,6 +9,11 @@ app = Quart(__name__)
 @app.route("/")
 async def hello():
     return "Hello world!"
+
+
+@app.route("/metrics")
+async def metrics():
+    return generate_latest()
 
 
 if __name__ == "__main__":
