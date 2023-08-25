57d1eff17186e4e56c6e17d58d8708ad07cfee70
あで <adelaide@fiberplane.com>
2023-08-25 11:20:07 +0200

    Try earlier release

--- start.py
+++ start.py
@@ -1,15 +1,8 @@
 from quart import Quart
-from autometrics import init
 
 from _01_start import _01_start
 from _02_error import _02_error
 
-init(
-    service_name="am-codesandbox",
-    version="0.0.1",
-    commit="123456",
-    branch="main",
-)
 app = Quart(__name__)
 app.register_blueprint(_01_start)
 app.register_blueprint(_02_error)
