f232297d57c103b23710c158cb170a8179d09f03
あで <adelaide@fiberplane.com>
2023-08-25 13:09:09 +0200

    Fix an oopsie

--- start.py
+++ start.py
@@ -2,10 +2,12 @@ from quart import Quart
 
 from _01_start import _01_start
 from _02_error import _02_error
+from _03_slo import _03_slo
 
 app = Quart(__name__)
 app.register_blueprint(_01_start)
 app.register_blueprint(_02_error)
+app.register_blueprint(_03_slo)
 
 if __name__ == "__main__":
     app.run()
